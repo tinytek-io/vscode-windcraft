2f15d9ee0904762b21978953980c72416cf12ab6
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-05-31 22:53:03 +0200

    Only emit logs in development mode

--- packages/ts-plugin/src/lib/log.ts
+++ packages/ts-plugin/src/lib/log.ts
@@ -1,15 +1,19 @@
-import * as fs from "fs";
-import * as path from "path";
+import fs from "node:fs";
+import path from "node:path";
 
 const logFilePath = path.join(__dirname, "..", "..", "log.txt");
 
-// Clear the log file
-fs.writeFileSync(logFilePath, "");
+const isDevelopment = process.env.NODE_ENV !== "production";
 
-export const log = (message: string) => {
+// Clear the log file
+if (isDevelopment) {
+  fs.writeFileSync(logFilePath, "");
+}
+
+export const log = isDevelopment ? (message: string) => {
   const date = new Date();
   fs.appendFileSync(
     logFilePath,
     `${date.toISOString()} [TS Plugin] - ${message}\n`
   );
-};
+} : () => {};
