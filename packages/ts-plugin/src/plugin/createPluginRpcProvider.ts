bd7badd7f428759ecf61a054ddcdf75a5da0fbed
Morten N.O. Nørgaard Henriksen <1136718+raix@users.noreply.github.com>
2024-06-11 19:57:36 +0200

    Add a bit of logging in the PluginRpcProvider

--- packages/ts-plugin/src/plugin/createPluginRpcProvider.ts
+++ packages/ts-plugin/src/plugin/createPluginRpcProvider.ts
@@ -1,10 +1,12 @@
 import ts from "typescript/lib/tsserverlibrary";
 import { getSimpleAST } from "../ast/SimpleAST/getSimpleAST";
 import { getClassNames } from "../ast/ClassNames/getClassNames";
+import { logger } from "@windcraft/utilities/logger/logger";
 
 export function createPluginRpcProvider(languageService: ts.LanguageService, typescript: typeof ts) {
   return {
     getSimpleAST: (fileName: string) => {
+      logger.log("Getting simple AST");
       const program = languageService.getProgram();
       if (!program) {
         throw new Error("Program not found")
@@ -12,6 +14,7 @@ export function createPluginRpcProvider(languageService: ts.LanguageService, typ
       return getSimpleAST(fileName, program, typescript);
     },
     getClassNames: (fileName: string, position: number) => {
+      logger.log("Getting class names");
       const program = languageService.getProgram();
       if (!program) {
         throw new Error("Program not found")
